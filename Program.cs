7eb29fb609efa466ba5066d165d456e934c7d13a
Savelya <savabezenkov@mail.ru>
2020-03-21 12:01:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace sixth_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            str = System.Text.RegularExpressions.Regex.Replace(str, @"\s+", " ");
+            string[] words = str.Split(" ");
+            Array.Sort(words);
+            Console.WriteLine(String.Join(" ", words));
+            Console.WriteLine(String.Concat(words.Select((x) => x.Last()).ToArray()));
+        }
+    }
+}
